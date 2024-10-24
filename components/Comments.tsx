9c9b5c9e1158f94ebbde56e8ad0f511994481828
Jatin Bhende <jatinbhende@gmail.com>
2024-10-24 14:59:03 +0530

    Removed sentry

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,34 +1,36 @@
-import { cn } from '@/lib/utils';
-import { useIsThreadActive } from '@liveblocks/react-lexical';
-import { Composer, Thread } from '@liveblocks/react-ui';
-import { useThreads } from '@liveblocks/react/suspense';
-import React from 'react'
+import { cn } from "@/lib/utils";
+import { useIsThreadActive } from "@liveblocks/react-lexical";
+import { Composer, Thread } from "@liveblocks/react-ui";
+import { useThreads } from "@liveblocks/react/suspense";
+import React from "react";
 
 const ThreadWrapper = ({ thread }: ThreadWrapperProps) => {
-    const isActive = useIsThreadActive(thread.id);
+  const isActive = useIsThreadActive(thread.id);
 
-    return (
-        <Thread 
-            thread={thread}
-            data-state = { isActive ? 'active' : null }
-            className={cn('comment-thread border',
-                isActive && 'border-blue-500 shadow-md',
-                thread.resolved && 'opacity-40')
-            }
-        />
-    )
-}
+  return (
+    <Thread
+      thread={thread}
+      data-state={isActive ? "active" : null}
+      className={cn(
+        "comment-thread border",
+        isActive && "border-blue-500 shadow-md",
+        thread.resolved && "opacity-40"
+      )}
+    />
+  );
+};
+
 const Comments = () => {
   const { threads } = useThreads();
   return (
-    <div className='comments-container'>
-        <Composer className='comment-composer' />
+    <div className="comments-container">
+      <Composer className="comment-composer" />
 
-        {threads.map((thread) => (
-            <ThreadWrapper key={thread.id} thread={thread} />
-        ))}
+      {threads.map((thread) => (
+        <ThreadWrapper key={thread.id} thread={thread} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
